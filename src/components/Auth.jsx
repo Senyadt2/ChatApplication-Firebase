de8994944917faaa9b89ba13a8435e67d8041636
Rikin Tuladhar <110732947+Senyadt2@users.noreply.github.com>
2024-03-17 16:26:07 +0545

    Update Auth.jsx

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -21,7 +21,8 @@ const Auth = ({setIsAuth}) => {
   return (
     <div className="auth">
       <p>Sign in with google to continue</p>
-      <p>Dont worry you google account's data will not be stored!</p>
+      <p>Dont worry, Your google's account data will not be stored!</p>
+      <p>Feel Free To Chat</p>
       <button style={{padding:"5px 10px",backgroundColor:"#F3G3DE"}} onClick={signInWithGoogle}>Sign In With Google</button>
       
     </div>
